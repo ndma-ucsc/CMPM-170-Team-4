87ee9e51a548445be51f16b0b2d03ed4251596a0
coagulantLegume <alvwolf@ucsc.edu>
2020-10-24 17:58:26 -0700

    Basic Projectile prefab added, test immediate attack system added

---
+++ CMPM 170 Team 4/Assets/Scripts/PlayerAttackSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackSystem : MonoBehaviour
+{
+    public GameObject opponent;
+    public Projectile attackType1;
+
+    void Update()
+    {
+        // temp test using e for immediate attack spawn
+        if(Input.GetKeyDown("space"))
+        {
+             Projectile attack = Instantiate(attackType1) as Projectile;
+             attack.init(transform.position, opponent.transform.position, 25);
+             attack.transform.parent = this.transform;
+        }
+    }
+}
