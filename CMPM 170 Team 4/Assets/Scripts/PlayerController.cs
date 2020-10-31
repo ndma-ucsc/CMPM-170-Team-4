9653404dd5d62d8e23bcaabd2b2af1f764c19d2d
Nathan Ma <ndma@ucsc.edu>
2020-10-31 16:48:38 -0700

    Start bounds

--- CMPM 170 Team 4/Assets/Scripts/PlayerController.cs
+++ CMPM 170 Team 4/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 18;
     private Rigidbody rig;
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
     
     private void Start() {
         rig = GetComponent<Rigidbody>();
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<MeshRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<MeshRenderer>().bounds.extents.y; //extents = size of height / 2
     }
     
     private void Update() {
         float hAxis, vAxis;
+        
         if (gameObject.name == "Player 1"){
             hAxis = Input.GetAxis("Horizontal1");
             vAxis = Input.GetAxis("Vertical1");
@@ -23,7 +31,6 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 movement = new Vector3(hAxis, vAxis, 0) * speed * Time.deltaTime;
-
         rig.MovePosition(transform.position + movement);
     }
 }
